10ad70c1cf32b7e18b487e285b258cd7fc9d8d94
cahegigithub <chernandezgil@gmail.com>
2016-06-13 12:09:06 +0200

    Widened app settings and fixed CoordinatorLayout slide problem with Huawei

--- app/src/main/java/com/carlos/capstone/SettingsActivity.java
+++ app/src/main/java/com/carlos/capstone/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.carlos.capstone;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
@@ -21,29 +24,27 @@ import com.carlos.capstone.utils.Utilities;
  */
 
 
-
-
 public class SettingsActivity extends PreferenceActivity
-        implements Preference.OnPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
 
-    private static final String LOG_TAG=SettingsActivity.class.getSimpleName();
+    private static final String LOG_TAG = SettingsActivity.class.getSimpleName();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        Toolbar toolBar=(Toolbar) findViewById(R.id.toolbar_activity_settings);
+        Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar_activity_settings);
         toolBar.setTitle(getString(R.string.settings_title));
         toolBar.setTitleTextColor(Color.WHITE);
 
         //http://developer.android.com/intl/es/reference/android/support/v7/appcompat/R.drawable.html
         //by default the color is pale grey
-        final Drawable upArrow= ContextCompat.getDrawable(this,R.drawable.abc_ic_ab_back_mtrl_am_alpha); //23.3
-      //  final Drawable upArrow= ContextCompat.getDrawable(this,R.drawable.ic_action_back);
+        final Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.abc_ic_ab_back_mtrl_am_alpha); //23.3
+        //  final Drawable upArrow= ContextCompat.getDrawable(this,R.drawable.ic_action_back);
         //final Drawable upArrow= ContextCompat.getDrawable(this,R.drawable.abc_ic_ab_back_material); //23.2
         //tint the arrow to white
-        upArrow.setColorFilter(ContextCompat.getColor(this,R.color.white), PorterDuff.Mode.SRC_ATOP);
+        upArrow.setColorFilter(ContextCompat.getColor(this, R.color.white), PorterDuff.Mode.SRC_ATOP);
         toolBar.setNavigationIcon(upArrow);
-
 
 
         toolBar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -61,13 +62,19 @@ public class SettingsActivity extends PreferenceActivity
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_animation_key)));
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_share_key)));
 
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_email_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_share_app_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_version_key)));
+
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        Utilities.analyticsTrackScreen(getApplication(),LOG_TAG);
+        Utilities.analyticsTrackScreen(getApplication(), LOG_TAG);
 
     }
+
     /**
      * Attaches a listener so the summary is always updated with the preference value.
      * Also fires the listener once, to initialize the summary (so it shows up before the value
@@ -76,12 +83,13 @@ public class SettingsActivity extends PreferenceActivity
     private void bindPreferenceSummaryToValue(Preference preference) {
         // Set the listener to watch for value changes.
         preference.setOnPreferenceChangeListener(this);
+        preference.setOnPreferenceClickListener(this);
 
-        if(preference instanceof CheckBoxPreference) {
+        if (preference instanceof CheckBoxPreference) {
             onPreferenceChange(preference,
                     PreferenceManager
                             .getDefaultSharedPreferences(preference.getContext())
-                            .getBoolean(preference.getKey(),false));
+                            .getBoolean(preference.getKey(), false));
         } else {
             // Trigger the listener immediately with the preference's
             // current value.
@@ -105,13 +113,58 @@ public class SettingsActivity extends PreferenceActivity
                 preference.setSummary(listPreference.getEntries()[prefIndex]);
             }
         } else if (preference instanceof CheckBoxPreference) {
-            String summaryValue=stringValue.equals("true")?"enabled":"disabled";
-            preference.setSummary(summaryValue);
-        }else {
-            // For other preferences, set the summary to the value's simple string representation.
-            preference.setSummary(stringValue);
+            if (preference.getKey() == getString(R.string.pref_animation_key)) {
+                String summaryValue = stringValue.equals("true") ? "enabled" : "disabled";
+                preference.setSummary(summaryValue);
+            }
+        } else {
+            String key = preference.getKey();
+            //we take the versionName and assign it to the pref var.
+            if (key == getString(R.string.pref_version_key)) {
+                String versionName;
+                try {
+                    versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+
+                } catch (PackageManager.NameNotFoundException e) {
+                    versionName = null;
+                }
+                preference.setSummary(versionName);
+
+            } else if (key == getString(R.string.pref_share_app_key)) {
+
+                preference.setSummary(getString(R.string.share_app_string));
+
+            } else if (key == getString(R.string.pref_email_key)) {
+
+                preference.setSummary(getString(R.string.email_string));
+
+            } else {
+                // For other preferences, set the summary to the value's simple string representation.
+                preference.setSummary(stringValue);
+            }
         }
         return true;
     }
 
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+
+        String key = preference.getKey();
+        if (key.equals(getString(R.string.pref_email_key))) {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",
+                    getString(R.string.mailto), null));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject));
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+
+
+        } else if (key.equals(getString(R.string.pref_share_app_key))) {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            //https://play.google.com/store/apps/details?id=com.carlos.capstone
+            sendIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.app_play_store_address));
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+        }
+        return true;
+    }
 }
